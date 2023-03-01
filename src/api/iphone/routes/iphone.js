81066ac96e34a9c0dc7038c39e4a8ff1f8bfc10b
faizan7744 <fs6820789@gmail.com>
2023-03-02 01:44:03 +0530

    changes

---
+++ src/api/iphone/routes/iphone.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * iphone router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::iphone.iphone');
---
+++ src/api/iphone/services/iphone.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * iphone service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::iphone.iphone');
